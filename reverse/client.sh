8d23eaa5fee5264200c3695cfad4f1b93b322591
Felix Geisendörfer <felix@debuggable.com>
2012-11-20 14:10:50 +0100

    verbose

--- reverse/client.sh
+++ reverse/client.sh
@@ -5,5 +5,5 @@ source "common.sh"
 echo "--> Setting local udp -> server tcp routing via: ${client_navdata_fifo} ..."
 rm -rf "${client_navdata_fifo}"
 mkfifo "${client_navdata_fifo}"
-nc -l -u "${client_localhost}" "${client_udp_navdata_port}" < "${client_navdata_fifo}" \
-  | nc "${client_localhost}" "${client_tcp_navdata_port}" > "${client_navdata_fifo}"
+nc -vl -u "${client_localhost}" "${client_udp_navdata_port}" < "${client_navdata_fifo}" \
+  | nc -v "${client_localhost}" "${client_tcp_navdata_port}" > "${client_navdata_fifo}"
--- reverse/server.sh
+++ reverse/server.sh
@@ -7,8 +7,8 @@ client_ssh="${1}"
 echo "--> Setting server tcp -> drone udp routing via: ${server_navdata_fifo} ..."
 rm -rf "${server_navdata_fifo}"
 mkfifo "${server_navdata_fifo}"
-nc -l "${server_localhost}" "${server_tcp_navdata_port}" < "${server_navdata_fifo}" \
-  | nc -u "${drone_ip}" "${drone_navdata_port}" > "${server_navdata_fifo}" &
+nc -vl "${server_localhost}" "${server_tcp_navdata_port}" < "${server_navdata_fifo}" \
+  | nc -vu "${drone_ip}" "${drone_navdata_port}" > "${server_navdata_fifo}" &
 
 
 echo "--> Setting up reverse ssh tunnel to: ${client_ssh} ..."
