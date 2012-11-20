7b96bd555ce1c8cb829163882e404555645c240e
Felix Geisendörfer <felix@debuggable.com>
2012-11-20 12:08:20 +0100

    Initial client stuff

---
+++ reverse_drone_client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ue # exit on error or unbound variable
+source common.sh
+
+echo "--> Creating ${client_navdata_fifo} ..."
+rm -rf "${client_navdata_fifo}"
+mkfifo "${client_navdata_fifo}"
+
+echo "--> Forwarding local navdata udp port: ${client_udp_navdata_port} via local tcp port ${client_tcp_navdata_port} to server ..."
+nc -l -u "${client_udp_navdata_port}" < "${client_navdata_fifo}" | nc "${client_localhost}" "${client_tcp_navdata_port}" > "${client_navdata_fifo}"
